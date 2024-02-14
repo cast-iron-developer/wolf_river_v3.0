f73d30e68f3ef8b286fbfd3bc796e34be08d01b2
Sam O'Donnell (Oh-Dee) <sodonnell5214@gmail.com>
2024-02-14 17:26:14 -0500

    Setting the scaffolding for the new application.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+  import "../app.scss";
+  import Navigation from "../lib/components/globals/navigation.svelte";
+</script>
+
+<main>
+  <Navigation></Navigation>
+  <slot></slot>
+</main>
